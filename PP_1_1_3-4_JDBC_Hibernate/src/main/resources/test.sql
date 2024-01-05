5bcdc9cafb1860c20eb449e635b41a5e84af8bba
Artyom Burnaev <149198360+MacheteNurbaev@users.noreply.github.com>
2024-01-05 19:02:16 +0300

    Add files via upload

---
+++ PP_1_1_3-4_JDBC_Hibernate/src/main/resources/test.sql
@@ -0,0 +1,16 @@
+##                                       базовые CRUD операции
+
+##  Добавить в юзерс в поля имя возраст маил хначения такие то:
+#insert into users (name, age, mail) values('ALex', 22, 'alex@mail.ru');
+
+##  Показать всех пользователей где id 1
+#select * from users where id = 1;
+
+##  Обновить(замени) такие то данные с такими то значением id
+#update users set name= 'Steve', age = 55 where id = 2;
+
+##  Удалить из "база данных" конкретные where
+#delete from users where name = 'Alex';
+
+
+
